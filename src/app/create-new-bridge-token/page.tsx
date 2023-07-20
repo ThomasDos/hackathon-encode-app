74e8732baaa783d186adefe25ba11406ebab3d39
Thomas Dos Anjos <dosanjos.thomas@gmail.com>
2023-07-20 11:54:56 +0200

    fix field form

--- src/app/create-new-bridge-token/page.tsx
+++ src/app/create-new-bridge-token/page.tsx
@@ -58,12 +58,12 @@ function CreateNewBridgeToken() {
 
         {!receipt && (
           <div className='flex flex-col'>
-            <label htmlFor='token-address' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+            <label htmlFor='token-receipt' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
               Put the transaction hash of token approval
             </label>
             <input
               type='text'
-              id='token-address'
+              id='token-receipt'
               className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
               required
               placeholder='0x...'
