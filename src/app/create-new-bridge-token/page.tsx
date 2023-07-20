567716ef3f2afb4e5f79fb958f0218d27123b52f
Thomas Dos Anjos <dosanjos.thomas@gmail.com>
2023-07-20 11:56:22 +0200

    hardcode rpc sepolia

--- src/app/create-new-bridge-token/page.tsx
+++ src/app/create-new-bridge-token/page.tsx
@@ -21,7 +21,7 @@ function CreateNewBridgeToken() {
   }
   const handleRetrieveReceipt = async () => {
     if (!chain) return
-    const provider = getRpcProvider(chain?.rpcUrls.default.http[0])
+    const provider = getRpcProvider('https://rpc.sepolia.org')
     const receipt = await provider.getTransactionReceipt(transactionHash)
     setReceipt(receipt)
     toast.success('Receipt retrieved')
